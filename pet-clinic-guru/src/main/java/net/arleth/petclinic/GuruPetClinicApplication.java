520729117f3a78a8b6a92ea9cf1bf3560eaa8d7b
Søren Arleth <soren@arleth.me>
2021-09-26 13:38:50 +0200

    added pet-clinic

---
+++ pet-clinic-guru/src/main/java/net/arleth/petclinic/GuruPetClinicApplication.java
@@ -0,0 +1,13 @@
+package net.arleth.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruPetClinicApplication.class, args);
+    }
+
+}
---
+++ pet-clinic-guru/src/test/java/net/arleth/petclinic/GuruPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.arleth.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
