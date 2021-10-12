ddf1a96a68156890968025696ac203dfa7931d81
Søren Arleth <soren@arleth.net>
2021-10-12 20:45:59 +0200

    MappedSuperClass on BaseEntity

--- pet-clinic-guru/pet-clinic-data/src/main/java/net/arleth/pet/model/BaseEntity.java
+++ pet-clinic-guru/pet-clinic-data/src/main/java/net/arleth/pet/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package net.arleth.pet.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = -7227986900235030780L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
